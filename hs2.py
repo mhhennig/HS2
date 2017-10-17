a459fb641c33141ee0bfce43a77428e5c48eaf37
martinosorb <martino.sorbaro@ed.ac.uk>
2017-10-17 16:08:35 +0100

    probe class, generalised clustering, etc.

--- hs2.py
+++ hs2.py
@@ -7,14 +7,33 @@ from sklearn.decomposition import PCA
 
 
 class herdingspikes(object):
-    def __init__(self, classpath=None, datapath=None):
-        self.datapath = datapath
-        if classpath is not None:
-            #  we want this to load a preexisting instantiation
-            #  of detected stuff
-            raise NotImplementedError()
+    def __init__(self):
+        print("""You can now load your data in three ways: with LoadDetected()
+        (automatically loads ProcessedSpikes, neighbormatrix and positions);
+        with DetectFromRaw (takes a path to the raw data and all detection
+        parameters) or with LoadH5 (takes a previously saved instance of
+        this class)""")
 
-    def LoadDetected(self):
+    # def SaveH5(self, filename):
+    #     store = pd.HDFStore(filename)
+    #     store['spikes'] = self.spikes
+    #     if self.IsClustered:
+    #         store['clusters'] = self.clusters
+    #     store.close()
+    #
+    # def LoadH5(self, filename):
+    #     store = pd.HDFStore(filename)
+    #     self.spikes = store['spikes']
+    #     if 'clusters' in store.keys():
+    #         self.clusters = store['clusters']
+    #         self.IsClustered = True
+    #         self.NClusters = len(self.clusters)
+    #         assert np.max(self.spikes.cl)+1 == self.NClusters
+    #     else:
+    #         self.IsClustered = False
+    #     store.close()
+
+    def LoadDetected(self, probe):
         sp = np.loadtxt('ProcessedSpikes')
         self.spikes = pd.DataFrame({'ch': sp[:, 0].astype(int),
                                     't': sp[:, 1].astype(int),
@@ -23,35 +42,25 @@ class herdingspikes(object):
                                     'y': sp[:, 4],
                                     'Shape': list(sp[:, 5:])
                                     })
-        # save neighbour matrix and positions for the chip geometry
-        f = open('neighbormatrix', 'r')
-        self.neighs = []
-        for l in f.readlines():
-            self.neighs.append(np.array(l[:-2].split(',')).astype(int))
-        f.close()
-        f = open('positions', 'r')
-        self.pos = []
-        for l in f.readlines():
-            self.pos.append(np.array(l[:-2].split(',')).astype(int))
-        f.close()
+        self.probe = probe
         self.IsClustered = False
 
-    def DetectFromRaw(self, num_channels, num_recording_channels, spike_delay,
-                      spike_peak_duration, noise_duration, noise_amp,
-                      max_neighbors, to_localize, cutout_length, fps, threshold,
+    def DetectFromRaw(self, datapath, probe,
+                      to_localize, cutout_length, threshold,
                       maa=0, maxsl=12, minsl=3, ahpthr=0):
-        detectData(self.datapath, num_channels, num_recording_channels,
-                   spike_delay, spike_peak_duration, noise_duration, noise_amp,
-                   max_neighbors, to_localize, cutout_length, fps, threshold,
+        detectData(datapath, probe.num_channels, probe.num_recording_channels,
+                   probe.spike_delay, probe.spike_peak_duration,
+                   probe.noise_duration, probe.noise_amp, probe.max_neighbors,
+                   to_localize, cutout_length, probe.fps, threshold,
                    maa, maxsl, minsl, ahpthr)
         # reload data into memory
-        self.LoadDetected()
+        self.LoadDetected(probe)
 
-    def PlotTracesChannels(self, eventid, ax=None):
+    def PlotTracesChannels(self, datapath, eventid, ax=None):
         if ax is None:
             ax = plt.gca()
-        pos, neighs = self.pos, self.neighs
-        data = np.fromfile(self.datapath,
+        pos, neighs = self.probe.positions, self.probe.neighbors
+        data = np.fromfile(datapath,
                            dtype=np.int16).reshape((1800000, 385))
 
         event = self.spikes.loc[eventid]
@@ -104,32 +113,32 @@ class herdingspikes(object):
             for cl in range(self.NClusters):
                 ax.annotate(str(cl), [ctr_x[cl], ctr_y[cl]], fontsize=16)
 
-    def CombinedMeanShift(self, bandwidth, alpha,
-                          pca_ncomponents=2, pca_whiten=True,
-                          bin_seeding=True, min_bin_freq=10, n_jobs=1):
+    def CombinedClustering(self, alpha, clustering_algorithm=MeanShift,
+                           pca_ncomponents=2, pca_whiten=True,
+                           **kwargs):
         pca = PCA(n_components=pca_ncomponents, whiten=pca_whiten)
         cutouts_pca = pca.fit_transform(np.array(list(self.spikes.Shape)))
         fourvec = np.vstack(([self.spikes.x], [self.spikes.y],
                              alpha*cutouts_pca.T)).T
-        ms = MeanShift(bandwidth=bandwidth, bin_seeding=bin_seeding,
-                       min_bin_freq=min_bin_freq, n_jobs=n_jobs)
-        ms.fit(fourvec)
-        self.spikes.cl = ms.labels_
-        self.NClusters = len(np.unique(ms.labels_))
-        assert np.max(self.spikes.cl)+1 == self.NClusters
-        assert len(ms.cluster_centers_) == self.NClusters
+
+        clusterer = clustering_algorithm(**kwargs)
+        clusterer.fit(fourvec)
+        self.spikes.cl = clusterer.labels_
+        self.NClusters = len(np.unique(clusterer.labels_))
+        # assert np.max(self.spikes.cl)+1 == self.NClusters
         print("Number of estimated clusters:", self.NClusters)
-        dic_cls = {'ctr_x': ms.cluster_centers_[:, 0],
-                   'ctr_y': ms.cluster_centers_[:, 1],
+
+        in_cl = []
+        for i in np.arange(self.NClusters):
+            in_cl.append(list(np.where(self.spikes.cl == i)[0]))
+        centers = np.asarray([np.mean(fourvec[cl], axis=0) for cl in in_cl])
+        dic_cls = {'ctr_x': centers[:, 0],
+                   'ctr_y': centers[:, 1],
                    'Color': 1.*np.random.permutation(
-                        self.NClusters)/self.NClusters}
-        # in_cl = []
-        # for i in np.arange(ms.cluster_centers_.shape[1]):
-        #     in_cl.append(list(np.where(self.spikes.cl == i)[0]))
-        # dic_cls['Content'] = in_cl
-        # dic_cls['AvgAmpl'] = [np.mean(self.spikes.Amplitude[cl])
-        #                       for cl in in_cl]
-        # dic_cls['Size'] = [len(cl) for cl in in_cl]
+                        self.NClusters)/self.NClusters,
+                   'Size': [len(cl) for cl in in_cl],
+                   'AvgAmpl': [np.mean(self.spikes.Amplitude[cl])
+                               for cl in in_cl]}
         self.clusters = pd.DataFrame(dic_cls)
         self.IsClustered = True
 
