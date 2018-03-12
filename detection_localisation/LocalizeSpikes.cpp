dbff45d0d930d1af00fe4ef0c5517c47eb915d76
martinosorb <martino.sorbaro@ed.ac.uk>
2018-03-12 18:17:35 +0000

    Actual merge of LocalizeSpikes.cpp, which was broken

--- detection_localisation/LocalizeSpikes.cpp
+++ detection_localisation/LocalizeSpikes.cpp
@@ -70,84 +70,6 @@ tuple<float, float> centerOfMass(deque<tuple<int, int> > centered_amps)
 
 tuple<float, float> localizeSpike(Spike spike_to_be_localized)
 {
-<<<<<<< HEAD
-	/*Estimates the X and Y position of where a spike occured on the probe.
-
-	Parameters
-	----------
-	spike_to_be_localized: Spike
-		The spike that will be used to determine where the origin of the spike occurred.
-
-	Returns
-	-------
-	position: tuple<float, float>
-		An X and Y coordinate tuple that corresponds to where the spike occurred.
-	*/
-	deque<tuple<int, int>> amps;
-	int curr_largest_amp = INT_MIN; //arbitrarily small to make sure that it is immediately overwritten
-	int curr_neighbor_channel;
-	int curr_amp;
-
-	int amp_cutout_size = spike_to_be_localized.amp_cutouts.size();
-	int neighbor_frame_span = Parameters::spike_delay * 2 + 1;
-	int neighbor_count = amp_cutout_size / neighbor_frame_span;
-	int matrix_offset = 0;
-    amps.push_back(make_tuple(spike_to_be_localized.channel, spike_to_be_localized.amplitude));
-
-	for (int i = 0; i < neighbor_count; i++) {
-		curr_neighbor_channel = Parameters::inner_neighbor_matrix[spike_to_be_localized.channel][i];
-        if(Parameters::masked_channels[curr_neighbor_channel] != 0) {
-    		for (int j = 0; j < neighbor_frame_span; j++) {
-    			curr_amp = spike_to_be_localized.amp_cutouts.at(j + matrix_offset);
-    			if(curr_amp > curr_largest_amp) {
-    				curr_largest_amp = curr_amp;
-    			}
-    		}
-    		amps.push_back(make_tuple(curr_neighbor_channel, curr_largest_amp));
-    		curr_largest_amp = INT_MIN;
-    		matrix_offset += neighbor_frame_span;
-        }
-	}
-    // int do_median = 0;
-    // int median = 0;
-    // int amps_size = amps.size();
-    // if(do_median == 1) {
-    //     sort(begin(amps), end(amps), CustomLessThan()); //sort the array
-    //     //Find median of array
-    // 	if(amps_size % 2 == 0) {
-    // 		median = (get<1>(amps.at(amps_size/2)) + get<1>(amps.at(amps_size/2 + 1)))/2;
-    // 	}
-    // 	else {
-    // 		median = get<1>(amps.at(amps_size/2));
-    // 	}
-    // }
-	// //Center amplitudes
-    // deque<tuple<int, int>> centered_amps;
-    // if(amps_size != 1) {
-    // 	for(int i = 0; i < amps_size; i++) {
-    // 		int curr_neighbor = get<0>(amps.at(i));
-    // 		int curr_amp = get<1>(amps.at(i));
-    // 		int new_amp = curr_amp - median;
-    // 		//if(new_amp > 0) {
-    // 			//centered_amps.push_back(make_tuple(curr_neighbor, new_amp));
-    // 		//}
-    //         centered_amps.push_back(make_tuple(curr_neighbor, new_amp));
-    // 	}
-    // } else {
-    //     centered_amps.push_back(amps.at(0));
-    // }
-    //
-    // int centered_amps_size = centered_amps.size();
-    // //Subtracting median made all values equal to or smaller than 0
-    // if(centered_amps_size == 0) {
-    //     centered_amps = amps;
-    // }
-	tuple<float,float> position = centerOfMass(amps);
-	amps.clear();
-	//centered_amps.clear();
-
-	return position;
-=======
         /*Estimates the X and Y position of where a spike occured on the probe.
 
            Parameters
@@ -227,7 +149,6 @@ tuple<float, float> localizeSpike(Spike spike_to_be_localized)
         amps.clear();
         centered_amps.clear();
         return position;
->>>>>>> multiple-files
 }
 
 }
