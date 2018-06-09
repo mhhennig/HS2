8618cd8b83b2181d820eee758049275cae9a83c2
Matthias H Hennig <mhhennig@gmail.com>
2018-06-09 19:33:06 +0100

    Removed for now as it throws an error on a default venv

--- herdingspikes/__init__.py
+++ herdingspikes/__init__.py
@@ -1,5 +1,5 @@
 from herdingspikes import probe
-from herdingspikes.parameter_optimisation import OptimiseParameters
+#from herdingspikes.parameter_optimisation import OptimiseParameters
 from herdingspikes.hs2 import HSDetection, HSClustering
 
 __all__ = ['probe', 'OptimiseParameters', 'HSDetection', 'HSClustering']
