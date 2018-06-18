a5d1992d6a81bc83da7daf4e3ae12e71b1a72809
Matthias Hennig <mhhennig@gmail.com>
2018-06-18 22:55:38 +0100

    Adapt to new directory structure

--- scripts/detect_sort_biocam.py
+++ scripts/detect_sort_biocam.py
@@ -2,8 +2,8 @@
 # this example reads Biocam files
 import sys
 sys.path.append('..')
-from hs2 import HSDetection, HSClustering
-from probe import BioCam
+from herdingspikes.hs2 import HSDetection,HSClustering
+from herdingspikes.probe import BioCam
 from datetime import datetime
 
 
@@ -37,6 +37,7 @@ if __name__ == '__main__':
                         maa=0, maxsl=12, minsl=3, ahpthr=0, out_file_name=out_file,
                         save_all=False)
             H.DetectFromRaw()
+            sampling = H.probe.fps
             # garbage collect
             del H
             del Probe
@@ -53,4 +54,6 @@ if __name__ == '__main__':
         C.CombinedClustering(alpha=0.3, bandwidth=0.3, bin_seeding=False, cluster_subset=100000, n_jobs=-1)
         sorted_files = [f.replace('.brw', '_clustered.hdf5') for f in data_files]
         print('Time taken for sorting: ' + str(datetime.now() - startTime))
-    C.SaveHDF5(sorted_files)
+
+        # save all spikes in hdf5
+        C.SaveHDF5(sorted_files,sampling=sampling)
