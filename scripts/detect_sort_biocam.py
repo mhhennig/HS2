a9aba9e382fa558f2f4224b9456cc1ea576eb789
Matthias H Hennig <mhhennig@gmail.com>
2018-12-22 20:00:36 +0000

    A few features added to the example script

--- scripts/detect_sort_biocam.py
+++ scripts/detect_sort_biocam.py
@@ -1,35 +1,75 @@
 # Detect and sort multiple recordings
 # this example reads Biocam files
+
 import sys
+import glob
+
+# import locally (works without install)
 sys.path.append('..')
+sys.path.insert(0, '../herdingspikes/')
+sys.path.insert(0, '../')
+
 from herdingspikes.hs2 import HSDetection,HSClustering
 from herdingspikes.probe import BioCam
 from datetime import datetime
-
+import getopt
+import h5py
 
 if __name__ == '__main__':
-    data_path = '/path/to/P91_05_07_17_TestMotionSeq/raw/'
-    files = ['P91_05_07_17_basicshort_stim1_ctl.brw', 'P91_05_07_17_swn_stim2_ctl.brw', 'P91_05_07_17_motionbandseq0_stim4_ctl.brw']
-    data_files = [data_path+f for f in files]
+    
+    def printargs():
+        print('HS2.py -i <inputfile or file mask> [-m <mbf> -a <0/1> -p <0/1>]')
 
-    print('Analysing files: ', data_files)
+    if len(sys.argv)==1:
+        printargs()
+        sys.exit(2)
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:", ["ifile="])
+    except getopt.GetoptError:
+        printargs()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        print(opt, arg)
+        if opt == '-h':
+            printargs()
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            filemask = arg
+
+    try:
+        sfs = glob.glob(filemask)
+        print('File(s):')
+        for f in sfs:
+            print(f)
+    except:
+        print('cannot find ' + filemask)
+        sys.exit(2)
 
     # detection parameters
+    # these work for 7kHz Biocam/retina
     to_localize = True
-    cutout_start = 10
-    cutout_end = 30
+    cutout_start = 4
+    cutout_end = 20
     cutout_length = cutout_end+cutout_start+1
-    threshold = 16
+    threshold = 22
     num_com_centers = 2
-
+    # what to do
     detect_it = True
     sort_it = True
-
+            
+    if sfs[0].find('.hdf5')>0:
+        print('Re-clustering')
+        detect_it = False
+    else:
+        print('Detecting and clustering')
+            
     # Detect and localise
     if detect_it is True:
         print("detecting...")
         startTime = datetime.now()
-        for f in data_files:
+        for f in sfs:
             out_file = f.replace('.brw', '')
             print("detecting "+f)
             # make a probe object first, and then set up the detector
@@ -43,21 +83,30 @@ if __name__ == '__main__':
             del H
             del Probe
         print('Time taken for detection: ' + str(datetime.now() - startTime))
-   else:
-        Probe = BioCam(data_files[0])
-        sampling = Probe.fps
-        del Probe
+    else:
+        # get sampling rate to store in clustered file
+        f = h5py.File(sfs[0])
+        sampling = f['Sampling'].value
+        f.close()
+        print('Sampling rate:'+str(sampling))
 
     # cluster everything
     if sort_it is True:
         print("reading files for clustering...")
-        out_files = [f.replace('.brw', '.bin') for f in data_files]
+        if detect_it is True:
+            out_files = [f.replace('.brw', '.bin') for f in sfs]
+        else:
+            out_files = sfs
         C = HSClustering(out_files, cutout_length=cutout_length)
         C.ShapePCA(pca_ncomponents=2, pca_whiten=True)
         print("starting Mean Shift...")
         startTime = datetime.now()
-        C.CombinedClustering(alpha=0.3, bandwidth=0.3, bin_seeding=False, cluster_subset=100000, n_jobs=-1)
-        sorted_files = [f.replace('.brw', '_clustered.hdf5') for f in data_files]
+        # parameters work for Biocam geometry, 42um pitch
+        C.CombinedClustering(alpha=0.3, bandwidth=0.3, bin_seeding=False, cluster_subset=200000, n_jobs=-1)
+        if detect_it is True:
+            sorted_files = [f.replace('.brw', '_HS2_clustered.hdf5') for f in sfs]
+        else:
+            sorted_files = [f.replace('.hdf5', '_HS2_clustered.hdf5') for f in sfs]
         print('Time taken for sorting: ' + str(datetime.now() - startTime))
 
         # save all spikes in hdf5
