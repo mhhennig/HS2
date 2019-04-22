953752f3b9fe5ae569cdec7586f7bc109f5d8f74
Martino Sorbaro <martino.sorbaro@ed.ac.uk>
2019-04-22 14:49:48 +0100

    General cleaning

--- herdingspikes/__init__.py
+++ herdingspikes/__init__.py
@@ -1,6 +1,6 @@
 from herdingspikes import probe
 
-#from herdingspikes.parameter_optimisation import OptimiseParameters
+# from herdingspikes.parameter_optimisation import OptimiseParameters
 from herdingspikes.hs2 import HSDetection, HSClustering
 
-__all__ = ['probe', 'OptimiseParameters', 'HSDetection', 'HSClustering']
+__all__ = ["probe", "OptimiseParameters", "HSDetection", "HSClustering"]
